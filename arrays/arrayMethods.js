861de2d8292e9cd2d42c4f1b9f5803cec201b1e5
kosborn4 <Kevin.Osborn@macquarie.com>
2022-02-21 11:09:24 +1100

    Array filter

--- arrays/arrayMethods.js
+++ arrays/arrayMethods.js
@@ -9,3 +9,13 @@ Array.prototype.myReduce = function (callback, initialValue) {
     }
     return accumulator;
   };
+
+Array.prototype.myFilter = function (callback) {
+const output = [];
+for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this) === true) {
+    output.push(this[i]);
+    }
+}
+return output;
+};
